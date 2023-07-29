1ff0aebd2474a91707d1a653cbd2c77e664daf01
ville <ville.prami@aalto.fi>
2023-07-29 21:32:09 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        Stay updated! Join 60,000+ product managers receiving monthly updates
+        on: Product discovery and building what matters Measuring to ensure
+        updates are a success And much more! Email address email@company.com
+        Subscribe to monthly newsletter
+      </div>
+      <div>
+        Thanks for subscribing! A confirmation email has been sent to
+        ash@loremcompany.com. Please open it and click the button inside to
+        confirm your subscription. Dismiss message
+      </div>
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  )
+}
+
+export default App
